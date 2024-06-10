020024d73fd612faed6ae69fc7a2670cc497064e
yuvan304 <166113588+yuvan304@users.noreply.github.com>
2024-06-10 21:59:22 +0530

    kubernetes

---
+++ Kubernetes(K8S).sh
@@ -0,0 +1,15 @@
+Kubernetes:(K8S)
+
+structure: CNPCA
+
+CLUSTER-->NODE-->POD-->CONTAINER-->APP
+
+Kubernetes is doen't direct connect with CONTAINERS
+ --> It doesn't know  there are exist even container was present in POD.
+--> Kubernetes don't know about CONTAINERS.
+--> Its communicates with only PODS.
+
+
+Components:
+
+1.API SERVER:Its for communicating with cluster, it takes
